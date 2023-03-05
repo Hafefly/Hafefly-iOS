fd498c3605a0dd6f08ce63f8a1ca523709ed2ec6
Samy Mehdid <s.mahdid@univ-boumerdes.dz>
2023-03-05 09:44:06 +0100

    Initial Commit

---
+++ Hafefly/HafeflyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HafeflyApp.swift
+//  Hafefly
+//
+//  Created by apple on 5/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct HafeflyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
