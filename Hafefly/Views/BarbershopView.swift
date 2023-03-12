ea6bbfe1273cd727f0e412708ef224a1d3bda9c8
RMehdid <rmehdid@harbineldjazair.com>
2023-03-12 09:15:49 +0100

    implemented barbershop view and tinder card swipes for barbers

---
+++ Hafefly/Views/BarbershopView.swift
@@ -0,0 +1,20 @@
+//
+//  BarbershopView.swift
+//  Hafefly
+//
+//  Created by Samy Mehdid on 11/3/2023.
+//
+
+import SwiftUI
+
+struct BarbershopView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BarbershopView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarbershopView()
+    }
+}
